03c5f6b9a2022ab4ca020d54d56873adcc2c4e8f
nufa_sempai <nufa_sempai@mail.ru>
2023-03-21 11:56:56 +0300

    ДЗ 16 - Maven и Spring Boot + web - Выполнено

--- src/main/java/com/example/homework16/controller/CalculatorController.java
+++ src/main/java/com/example/homework16/controller/CalculatorController.java
@@ -24,36 +24,49 @@ public class CalculatorController {
     @GetMapping("/plus")
     public String plus (@RequestParam(value = "num1", required = false) Integer a,
                         @RequestParam(value = "num2", required = false) Integer b) {
-        return buildView (a, b, calculatorService.plus(a, b), "+");
+        return buildView (a, b, "+");
     }
 
     @GetMapping("minus")
     public String minus (@RequestParam(value = "num1", required = false) Integer a,
                          @RequestParam(value = "num2", required = false) Integer b) {
-        return buildView (a, b, calculatorService.minus(a, b), "-");
+        return buildView (a, b, "-");
     }
 
     @GetMapping("multiply")
     public String multiply (@RequestParam(value = "num1", required = false) Integer a,
                             @RequestParam(value = "num2", required = false) Integer b) {
-        return buildView (a, b, calculatorService.multiply(a, b), "*");
+        return buildView (a, b, "*");
     }
 
     @GetMapping("divide")
     public String divide (@RequestParam(value = "num1", required = false) Integer a,
                           @RequestParam(value = "num2", required = false) Integer b) {
-        return buildView (a, b, calculatorService.divide(a, b), "/");
+        return buildView (a, b, "/");
     }
 
     private String buildView(Integer a,
                              Integer b,
-                             Number result,
                              String operation) {
         if (a == null || b == null) {
             return "Не передан один из параметров";
         }
         if ("/".equals(operation) && b == 0) {
             return "Нельзя делить на 0";
+        }
+        Number result;
+        switch (operation) {
+            case "-":
+                result = calculatorService.minus(a, b);
+                break;
+            case "*":
+                result = calculatorService.multiply(a, b);
+                break;
+            case "/":
+                result = calculatorService.divide(a, b);
+                break;
+            default:
+                result = calculatorService.plus(a, b);
         }
         return a + " " + operation + " " + b + " = " + result;
     }
